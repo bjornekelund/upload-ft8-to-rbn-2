5a089ecd9afef16ee1f8a22bf147d8f8593309d5
Björn Ekelund <bjorn@ekelund.nu>
2019-03-30 07:15:53 +0100

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -11,4 +11,4 @@ SCL=`cat $XADC_PATH/in_temp0_scale`
 FORMULA="(($OFF+$RAW)*$SCL)/1000.0"
 VAL=`echo "scale=2;${FORMULA}" | bc`
 
-echo "in_temp0 = ${VAL} C"
+echo "Zynq 7010 die temperature = ${VAL}C"
